0cc56c1fd80acf27468df8d0ae91ee4b440f50ad
Joakim Bording <joakim@behalf.no>
2024-12-08 20:55:49 +0100

    Update schema types

--- apps/web/src/types.ts
+++ apps/web/src/types.ts
@@ -1,28 +1,3 @@
-export type Answer = {
-  id: string
-  type: 'Answer'
-  heading: string
-  value: string
-}
-
-export type QuestionBase = {
-  id: string
-  heading: string
-  text?: string // HTML
-  summary?: string
-  details?: string
-  options?: Answer[]
-  flow: 'stop' | 'continue' | null
-}
-
-export type Radio = QuestionBase & {
-  type: 'Radio'
-}
-
-export type Text = QuestionBase & {
-  type: 'Text'
-}
-
 export type Expression = {
   field: string
   operator:
@@ -41,13 +16,124 @@ export type Expression = {
   errorMessage?: string
 }
 
-export type Page = {
+type WithValidator = {
+  // Denne er mer kompleks i losen, tror vi kan forenkle denne i første runde og kun bruke den for input
+  validator?: {
+    pattern: string // Regex som f.eks "^\\d+(,\\d+)?$"
+    error: string
+  }
+}
+
+export type Answer = {
   id: string
-  type: 'Page' | 'Result'
+  type: 'Answer'
+  heading: string
+  value: string
+}
+
+export type Content = {
+  id: string
+  heading?: string
+  text?: string // HTML
+  summary?: string
+  details?: string
+  show?: Expression // Vi har også hide, men tenker vi kan forenkle det til kun vis i første runde.
+  flow?: 'stop' | 'continue' | null
+}
+
+export type Text = Content & {
+  type: 'Text'
+}
+
+export type Radio = Content & {
+  type: 'Radio'
+  options?: Answer[]
+}
+
+export type Select = Content & {
+  type: 'Select'
+  optional?: boolean // By default all fields are required.
+  options?: Answer[]
+}
+
+export type Checkbox = Content & {
+  type: 'Checkbox'
+  allMandatory?: boolean // Required user to check all the options in the list, in order for it to be "valid".
+  grid: boolean // Display options in a three column grid.
+  optional?: boolean // By default all fields are required.
+  options?: Answer[]
+}
+
+export type Input = Content &
+  WithValidator & {
+    type: 'Input'
+    optional?: boolean // By default all fields are required.
+  }
+
+export type NumberInput = Content &
+  WithValidator & {
+    type: 'Number'
+    minimum?: number
+    maximum?: number
+    step?: number // Defaults to 1
+    optional?: boolean // By default all fields are required.
+  }
+
+export type Information = {
+  id: string
+  type: 'Information'
+  heading: string
+  text?: string // HTML
+}
+
+export type Error = {
+  id: string
+  type: 'Error'
+  heading: string
+  text?: string // HTML
+}
+
+export type SimpleResult = {
+  // Brukes i branches for å vise negative resultatsider etter errors
+  id: string
+  type: 'Result'
+  heading: string
+  lead?: string // HTML
+}
+
+export type Branch = {
+  id: string
+  type: 'Branch'
+  test: Expression
+  children: (Information | Error | SimpleResult)[]
+}
+
+export type Intro = {
+  id: string
+  type: 'Intro'
+  heading: string
+  lead?: string // HTML
+  children?: Text[]
+}
+
+export type Result = {
+  id: string
+  type: 'Result'
   heading: string
   lead?: string // HTML
   show?: Expression
-  children?: (Radio | Text)[]
+  children?: Text[]
 }
 
-export type Wizard = Page[]
+export type PageChildren = Text | Radio | Checkbox | Select | Input | NumberInput | Branch
+
+export type Page = {
+  id: string
+  type: 'Page'
+  heading: string
+  lead?: string // HTML
+  show?: Expression
+  children?: PageChildren[]
+}
+
+export type Wizard = (Intro | Page | Result)[]
