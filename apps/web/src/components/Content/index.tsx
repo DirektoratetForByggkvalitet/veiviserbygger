a22924413cdc2751a0ea18b978acb3ac190c8295
Joakim Bording <joakim@behalf.no>
2024-12-12 15:57:17 +0100

    Content actions

--- apps/web/src/components/Content/index.tsx
+++ apps/web/src/components/Content/index.tsx
@@ -2,6 +2,8 @@ import { PageContent, Answer } from '@/types'
 import Input from '@/components/Input'
 import Editor from '@/components/Editor'
 import Button from '@/components/Button'
+import Dropdown from '@/components/Dropdown'
+import Checkbox from '@/components/Checkbox'
 
 import BEMHelper from '@/lib/bem'
 import styles from './Styles.module.scss'
@@ -14,6 +16,24 @@ type Props = {
 
 export default function Content({ type, data }: Props) {
   if (!data) return null
+
+  const contentActions = [
+    {
+      value: '0',
+      label: 'Flytt til annen side',
+      onClick: () => console.log('Flytt'),
+    },
+    {
+      value: '0',
+      label: 'Dupliser',
+      onClick: () => console.log('Flytt'),
+    },
+    {
+      value: '0',
+      label: 'Fjern spørsmål',
+      onClick: () => console.log('Fjern'),
+    },
+  ]
 
   const nodes: any = {
     Text: (data: any) => {
@@ -35,7 +55,10 @@ export default function Content({ type, data }: Props) {
     Radio: (data: any) => {
       return (
         <>
-          <h2 {...bem('title')}>Radio</h2>
+          <div {...bem('header')}>
+            <h2 {...bem('title')}>Spørsmål </h2>
+            <Dropdown icon="Ellipsis" direction="right" options={contentActions} simple />
+          </div>
           <Input
             label="Tittel"
             value={data?.heading || ''}
@@ -62,14 +85,58 @@ export default function Content({ type, data }: Props) {
           </ul>
           <Button type="button">Legg til svaralternativ</Button>
           {/* TODO: summary, details, show */}
+          <h3 {...bem('sub-title')}>Valg</h3>
+          <div {...bem('grid')}>
+            <Dropdown
+              value={'Radio'}
+              options={[
+                {
+                  value: 'Radio',
+                  label: 'Radioknapper',
+                },
+                {
+                  value: 'Checkbox',
+                  label: 'Sjekkbokser',
+                },
+                {
+                  value: 'Select',
+                  label: 'Nedtrekksmeny',
+                },
+              ]}
+            />
+            <Checkbox
+              label="Grid"
+              checked={data?.grid}
+              onChange={() => {
+                console.log('Hej')
+              }}
+            />
+            <Checkbox
+              label="Valgfritt"
+              checked={data?.optional}
+              onChange={() => {
+                console.log('Hej')
+              }}
+            />
+            <Checkbox
+              label="Alle påkrevd"
+              checked={data?.allMandatory}
+              onChange={() => {
+                console.log('Hej')
+              }}
+            />
+          </div>
         </>
       )
     },
     Branch: (data: any) => {
       return (
         <>
-          <h2 {...bem('title')}>Branch</h2>
-          {data?.children?.map((child: PageContent) => (
+          <div {...bem('header')}>
+            <h2 {...bem('title')}>Branch </h2>
+            <Dropdown icon="Ellipsis" direction="right" options={contentActions} simple />
+          </div>
+          {data?.content?.map((child: PageContent) => (
             <>{(nodes[child.type] || nodes.Fallback)(child)}</>
           ))}
         </>
@@ -78,7 +145,7 @@ export default function Content({ type, data }: Props) {
     Error: (data: any) => {
       return (
         <>
-          <h2 {...bem('title')}>Error</h2>
+          <h2 {...bem('title')}>Resultatboks </h2>
           <Input
             label="Tittel"
             value={data?.heading || ''}
@@ -87,6 +154,38 @@ export default function Content({ type, data }: Props) {
             }}
           />
           <Editor label="Innhold" value={data?.text} />
+        </>
+      )
+    },
+    Information: (data: any) => {
+      return (
+        <>
+          <div {...bem('header')}>
+            <h2 {...bem('title')}>Gi informasjon </h2>
+            <Dropdown icon="Ellipsis" direction="right" options={contentActions} simple />
+          </div>
+          <Input
+            label="Tittel"
+            value={data?.heading || ''}
+            onChange={() => {
+              console.log('Hej')
+            }}
+          />
+          <Editor label="Innhold" value={data?.text} />
+        </>
+      )
+    },
+    Result: (data: any) => {
+      return (
+        <>
+          <h2 {...bem('title')}>Resultat </h2>
+          <Input
+            label="Tittel"
+            value={data?.title || ''}
+            onChange={() => {
+              console.log('Hej')
+            }}
+          />
         </>
       )
     },
