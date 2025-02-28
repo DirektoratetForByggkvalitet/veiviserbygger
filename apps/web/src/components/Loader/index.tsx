f269b2bc9c30917838bf0d42d5e8c55ce4e7ea09
Joakim Bording <joakim@behalf.no>
2025-02-28 14:33:12 +0100

    New loader

--- apps/web/src/components/Loader/index.tsx
+++ apps/web/src/components/Loader/index.tsx
@@ -1,56 +1,14 @@
-import { useState } from 'react'
 import BEMHelper from '@/lib/bem'
 import styles from './Styles.module.scss'
 
 const bem = BEMHelper(styles)
 
-type Props = {
-  message?: string
-}
-
-const messages = [
-  'Laster',
-  'Loading',
-  'Cargando',
-  'Chargement',
-  'Laden',
-  'Caricamento',
-  'Laddar',
-  'Indlæser',
-  'Ladataan',
-  'Ładowanie',
-  'Загрузка',
-  '加载中',
-  'Läser in',
-  'Yükleniyor',
-  'Carregando',
-  'Încărcare',
-  'Načítání',
-  'Lataus',
-  'Kargante',
-  'Ielādē',
-  'Laddning',
-  'Uppfläds',
-  'Φόρτωση',
-  'Učitavanje',
-  'Лоадинг',
-]
-
-export default function Loader({
-  message = messages[Math.floor(Math.random() * messages.length)],
-}: Props) {
-  const [index, setIndex] = useState(0)
-
-  const msPrLetter = Math.floor(Math.max(50, 100 / message.length))
-  const messageLength = message.length
-
-  setInterval(() => {
-    setIndex((i) => (i + 1) % messageLength)
-  }, msPrLetter)
-
+export default function Loader() {
   return (
-    <div {...bem()} style={{ '--duration': `${message.length * 20}ms` }}>
-      <span {...bem('rotate')}>{message[index]}</span>
+    <div {...bem()}>
+      <span {...bem('box1')}></span>
+      <span {...bem('box2')}></span>
+      <span {...bem('label')}>Laster</span>
     </div>
   )
 }
