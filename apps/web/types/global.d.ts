0396ef1f55bfdb8a983c912b98089957785a9de2
Kristoffer Brabrand <kristoffer@brabrand.no>
2025-06-10 10:47:40 +0200

    Add jest setup and custom matcher for firestore refs

--- apps/web/types/global.d.ts
+++ apps/web/types/global.d.ts
@@ -1,7 +1,20 @@
+import { DocumentReference } from 'firebase/firestore'
 import 'react'
 
 declare module 'react' {
   interface CSSProperties {
     [key: `--${string}`]: string | undefined
+  }
+}
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toBeReferenceTo(expected: DocumentReference): R
+    }
+
+    interface Expect {
+      toBeReferenceTo(expected: DocumentReference): R
+    }
   }
 }
