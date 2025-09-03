a67910f7a64613350ab90e0925dbcb53a0b03f0d
Kristoffer Brabrand <kristoffer@brabrand.no>
2025-09-03 15:43:01 +0200

    Fix ACL issue when getting in the getImageUrl function
    When running in the emulator we shouldn't call the getSignedUrl function on the file. It triggers ACL checks even if it's not necessary/possible against an emulator, and an error is thrown. To support running in dev with an emulator we check if it's an emulator and call publicUrl instead.

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,12 @@
-import admin from 'firebase-admin'
+import admin, { AppOptions } from 'firebase-admin'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config({
   path: ['.env.local', '.env.development', '.env'],
   debug: true,
 })
+
+const projectId = process.env.PUBLIC_FIREBASE_PROJECT_ID ?? 'veiviserbygger'
 
 // Assuming you have your service account JSON as base64 in an env variable
 let credential: AppOptions['credential']
@@ -17,32 +19,42 @@ if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
   } catch (error) {
     console.error('Failed to parse GOOGLE_APPLICATION_CREDENTIALS:', error)
   }
-}
+} else if (process.env.PUBLIC_FIREBASE_EMULATOR_FIRESTORE_HOST) {
+  /**
+   * When using the Firestore emulator, the Admin SDK can be initialized without credentials.
+   * We set a few env vars that the admin SDK looks for when initializing in an emulator environment.
+   */
 
-admin.initializeApp({
-  ...(credential ? { credential } : {}),
-  storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET ?? 'veiviserbygger.appspot.com',
-})
-
-// Set up environment variables for Firebase emulators if they are defined
-if (process.env.PUBLIC_FIREBASE_EMULATOR_FIRESTORE_HOST) {
-  // Firestore Emulator
+  // Firestore emulator
   process.env.FIRESTORE_EMULATOR_HOST = `${process.env.PUBLIC_FIREBASE_EMULATOR_FIRESTORE_HOST}:${process.env.PUBLIC_FIREBASE_EMULATOR_FIRESTORE_PORT}`
 
-  // Auth Emulator
-  process.env.FIREBASE_AUTH_EMULATOR_HOST =
-    process.env.PUBLIC_FIREBASE_EMULATOR_AUTH_HOST || 'http://localhost:9099'
+  // Auth emulator
+  process.env.PUBLIC_FIREBASE_EMULATOR_AUTH_HOST || 'localhost:9099'
 
-  // Cloud Storage Emulator (if needed)
-  process.env.FIREBASE_STORAGE_EMULATOR_HOST = process.env.PUBLIC_FIREBASE_EMULATOR_STORAGE_HOST
-    ? `${process.env.PUBLIC_FIREBASE_EMULATOR_STORAGE_HOST}:${process.env.PUBLIC_FIREBASE_EMULATOR_STORAGE_PORT}`
+  // Storage emulator
+  process.env.STORAGE_EMULATOR_HOST = process.env.PUBLIC_FIREBASE_EMULATOR_STORAGE_HOST
+    ? `http://${process.env.PUBLIC_FIREBASE_EMULATOR_STORAGE_HOST}:${process.env.PUBLIC_FIREBASE_EMULATOR_STORAGE_PORT}`
     : 'http://localhost:9199'
 }
+
+// Initialize Admin SDK WITHOUT credentials
+admin.initializeApp({
+  ...(credential
+    ? {
+        // set credentials if we have them
+        credential,
+      }
+    : {
+        // ...otherwise, we're probably in an emulator environment. We still
+        // need to set the projectId, otherwise the Admin SDK will complain.
+        projectId,
+      }),
+  storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET ?? 'veiviserbygger.appspot.com',
+})
 
 import express from 'express'
 import setupServer from './server'
 import { getFirestore } from 'firebase-admin/firestore'
-import { AppOptions } from 'firebase-admin/app'
 import { getStorage } from 'firebase-admin/storage'
 import { getCache } from './services/cache'
 import { IS_JEST } from './constants'
