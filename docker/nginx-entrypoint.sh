c46b6ac7a31d29bd6ceb427a110d0304f7516237
Eirik Wulff <ew@dibk.no>
2025-09-03 15:04:29 +0200

    Error in nginx.conf.template reworked

--- docker/nginx-entrypoint.sh
+++ docker/nginx-entrypoint.sh
@@ -3,15 +3,15 @@
 # Ensure PORT is set (Heroku provides it, but a default is good for local testing)
 PORT=${PORT:-80}
 
-# Debugging: Print the PORT value to verify it’s set
+# # Debugging: Print the PORT value to verify it’s set
 echo "Using PORT: $PORT"
 
-# Replace ${PORT} in the template with the actual PORT value using sed
-sed -e "s/\${PORT}/$PORT/g" \
-    -e "s/\${PUBLIC_FIREBASE_PROJECT_ID}/$PUBLIC_FIREBASE_PROJECT_ID/g" \
+# # Replace ${PORT} in the template with the actual PORT value using sed
+sed -e "s/<PORT>/$PORT/g" \
+    -e "s/<PUBLIC_FIREBASE_PROJECT_ID>/$PUBLIC_FIREBASE_PROJECT_ID/g" \
     /etc/nginx/nginx.conf.template  > /etc/nginx/nginx.conf
 
-# Display the final configuration
+# # Display the final configuration
 cat /etc/nginx/nginx.conf
 
 # Start the API server (in the background) using the PORT value
