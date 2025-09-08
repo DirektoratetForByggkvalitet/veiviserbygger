f3090c14b5ccca9d421bff7a5c01ec3d5fc43014
Eirik Wulff <ew@dibk.no>
2025-09-08 09:34:55 +0200

    Adds .env at startup if it is missing

--- docker/nginx-entrypoint.sh
+++ docker/nginx-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo << EOF
+cat << EOF
 +-------------------------------+
 |  V E I V I S E R B Y G G E R  |
 +-------------------------------+
@@ -9,8 +9,7 @@ A Losen-compliant wizard builder,
 run in a tiny OCI container
 
 EOF
-# Replace environment variables in the nginx config template
-# Replace ${WEB_PORT} in the template with the actual PORT value using sed
+# Replace environment variables in the nginx config template using sed
 sed -e "s/\${WEB_PORT}/$WEB_PORT/g" \
     -e "s/\${PUBLIC_FIREBASE_APP_ID}/$PUBLIC_FIREBASE_APP_ID/g" \
     -e "s/\${LOCAL_PORT}/$LOCAL_PORT/g" \
@@ -24,6 +23,7 @@ echo ""
 echo "Starting the application..."
 
 # Start the API server (in the background) using the PORT value
+touch api/.env
 PORT=${LOCAL_PORT} npm run start --workspace=api &
 
 # Start Nginx
